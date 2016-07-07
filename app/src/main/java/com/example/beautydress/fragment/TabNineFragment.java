e8a3eb17eef3c94712645ee8405f57167d503e71
dongjincheng1 <2032088697@qq.com>
2016-07-07 22:01:19 +0800

    我们的美丽衣橱v.1.0 增加db 修改uri

--- app/src/main/java/com/example/beautydress/fragment/TabNineFragment.java
+++ app/src/main/java/com/example/beautydress/fragment/TabNineFragment.java
@@ -1,6 +1,5 @@
 package com.example.beautydress.fragment;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,24 +8,22 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.beautydress.R;
+import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
+import com.lidroid.xutils.http.client.HttpRequest;
 
 /**
  * Created by Djc on 2016/7/6.
  */
 public class TabNineFragment extends Fragment {
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-    }
+    private HttpUtils utils;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        utils=new HttpUtils();
     }
 
     @Nullable
@@ -34,5 +31,37 @@ public class TabNineFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=(View) inflater.inflate(R.layout.tab_9kuai9_layout,null);
         return view;
+    }
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        String urlStr = "";
+        utils.send(HttpRequest.HttpMethod.GET, urlStr, new RequestCallBack<String>() {
+
+            @Override
+            public void onFailure(HttpException arg0, String arg1) {
+
+
+            }
+
+            @Override
+            public void onSuccess(ResponseInfo<String> responseInfo) {
+                String jsonStr = responseInfo.result;
+//                try {
+//                    List<News> newsesPerTime = new LinkedList<>();
+//                    ParseJsonUtil.parseNews(jsonStr,newsesPerTime);
+//                    System.out.println("pageIndex = " + pageIndex
+//                            + "～～～～＞　" + newsesPerTime);
+//
+//                    newses.addAll(newsesPerTime);
+//                    myAdapter.notifyDataSetChanged();
+//                } catch (JSONException e) {
+//
+//                    e.printStackTrace();
+//                }
+
+            }
+        });
+
+        super.onActivityCreated(savedInstanceState);
     }
 }
