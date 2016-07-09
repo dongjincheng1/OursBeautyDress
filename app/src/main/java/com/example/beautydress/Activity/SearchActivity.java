c72e76d56b320676fe5791033e6f51ee07b41e56
heyzsm <1941966099@qq.com>
2016-07-09 16:07:23 +0800

    sm---首页Toolbar2

--- app/src/main/java/com/example/beautydress/Activity/SearchActivity.java
+++ app/src/main/java/com/example/beautydress/Activity/SearchActivity.java
@@ -1,15 +1,35 @@
 package com.example.beautydress.Activity;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.example.beautydress.R;
 
 public class SearchActivity extends AppCompatActivity {
-
+private Toolbar toolbar;
+    private ActionBar actionbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        toolbar = (Toolbar) findViewById(R.id.setting_toolbar);
+        setSupportActionBar(toolbar);
+        //获取到Toolbar对应的Actionbar
+        actionbar = getSupportActionBar();
+        //添加返回按钮
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setDisplayShowTitleEnabled(false);
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            //返回按钮
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
